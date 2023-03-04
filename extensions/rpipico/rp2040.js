3aee2b19eb0007168ace226548a8fd0346f0e4e7
Yeqin Gong <gon@gyeq.in>
2023-03-05 00:15:42 +0800

    Build for 5b454e836941c57fe29dc2ee8f67d36702cf1e07

---
+++ extensions/rpipico/rp2040.js
@@ -0,0 +1,1 @@
+importScripts(new URL("../pyboard/micropython.js",Scratch.extensions.url).href);class RP2040 extends MicroPython{constructor(){super()}async _onConnect(){await super._onConnect(),await this.send("import rp2")}}self.RP2040=RP2040;
